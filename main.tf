e4bfead2040ca08fc81894b8519eee98d38e8abf
eliohoyeck <eelhoyeck@LT-C02FD5QHMD6R.local>
2021-06-17 15:24:45 +0200

    Add remote backend and variable definitions

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
   required_version = "> 0.14"
+
+  backend "s3"  {
+  bucket = "circle-ci-backend-20210617130955417600000001"
+  key    = "terraform/webapp/terraform.tfstate"
+  region = "us-east-1"
+                }
+
 }
 
 provider "aws" {
